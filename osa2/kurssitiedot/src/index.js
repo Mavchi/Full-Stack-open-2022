f04f6e5ac466ba94d24619d60149c83d5a72636d
Mavchi <aleksi.khlebnikov@gmail.com>
2020-10-30 18:42:27 +0200

    2.5: erillinen moduuli

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,47 +1,7 @@
-// v2.3
+// v2.5
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = ({name}) => {
-  return (
-    <h3>{name}</h3>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    parts.map( part =>
-      <Part key={part.id} part={part} />
-    )
-  )
-}
-
-const Total = ({ parts }) => {
-  const values = parts.map( part => part.exercises )
-  const sum = (accumulator, currentValue) => accumulator + currentValue
-
-  return (
-    <p><strong>total of {values.reduce(sum)} exercises</strong></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
