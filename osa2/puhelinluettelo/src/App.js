6fe223446787278755cc2cce929fe5b1fb24dd50
Mavchi <aleksi.khlebnikov@gmail.com>
2020-10-31 19:07:03 +0200

    2.11: puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,6 @@
-// 2.10+
-import React, { useState } from 'react'
+// 2.11+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Person = ({ person }) => {
     return (
@@ -42,15 +43,18 @@ const PersonForm = (props) => {
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const HandleNameChange = (event) => {
       setNewName(event.target.value)
