8bd3ebe01906ce4c0c0cd4504bba0eb3a5d15277
Mavchi <aleksi.khlebnikov@gmail.com>
2022-05-13 11:38:57 +0300

    1.11*: unicafe step6 done

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,13 +1,41 @@
 import react from 'react'
 import { useState } from 'react'
 
+const Average = ({ feedback }) => {
+  let sum = feedback.good*1 + feedback.bad*-1
+  let total = feedback.good+feedback.neutral+feedback.bad
+
+  console.log(sum)
+  console.log(total)
+
+  return (
+    <div>
+      average {isNaN(sum/total) ? 0 : sum/total} 
+    </div>
+  )
+}
+
+const Positive = ({ feedback }) => {
+  let positive = feedback.good / (feedback.good+feedback.neutral+feedback.bad)
+
+  return (
+    <div>
+      positive {isNaN(positive) ? 0 : positive}
+    </div>
+  )
+}
+
 const ShowFeedback = ({ feedback }) => {
+
   return (
     <div>
       <h3>Statistics</h3>
-      <p>good {feedback.good}</p>
-      <p>neutral {feedback.neutral}</p>
-      <p>bad {feedback.bad}</p>
+      <div>good {feedback.good}</div>
+      <div>neutral {feedback.neutral}</div>
+      <div>bad {feedback.bad}</div>
+      <div>all {feedback.good+feedback.neutral+feedback.bad}</div>
+      <Average feedback={feedback} />
+      <Positive feedback={feedback} />
     </div>
   )
 }
