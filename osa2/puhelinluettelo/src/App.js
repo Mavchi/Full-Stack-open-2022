3e21a28b643ab92a8d23c1dfacfaad1ca913fab1
Mavchi <aleksi.khlebnikov@gmail.com>
2022-05-14 08:32:32 +0300

    2.7: puhelinluettelo step2 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,38 +4,48 @@ const Persons = ({persons}) => {
   //console.log(persons)
   return (
     persons.map( person => 
-      <p key={person.name}>{person.name}</p>
+      <p key={person.name}>{person.name} {person.number}</p>
     )
   )
 }
 
-
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNewName = (event) => {
     event.preventDefault()
     const newPerson = {
       name: newName,
+      number: newNumber,
     }
-    // see if input already stored
+    // see if name already stored
     let count = persons.filter(person => person.name===newName)
     if (count.length > 0){
       alert(`${newName} is already added to phonebook`)
       setNewName('')
+      setNewNumber('')
     }
     else {
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handleChangeInput = (event) => {
-    console.log(event.target.value)
+  const handleChangeName = (event) => {
+    //console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleChangeNumber = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -43,7 +53,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleChangeInput}/>
+          name: <input value={newName} onChange={handleChangeName}/>
+        </div>
+        <div>
+          name: <input value={newNumber} onChange={handleChangeNumber}/>
         </div>
         <div>
           <button onClick={handleNewName} type="submit">add</button>
