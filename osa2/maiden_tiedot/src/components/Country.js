72476dfc27a3fa733c98aa92bc6e3eaaa099ca44
Mavchi <aleksi.khlebnikov@gmail.com>
2022-05-24 19:03:42 +0300

    part 2 is now done for, all done

--- osa2/maiden_tiedot/src/components/Country.js
+++ osa2/maiden_tiedot/src/components/Country.js
@@ -1,4 +1,25 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Country = ({ country }) => {
+    const [weather, setWeather] = useState(null)
+    const api_key = process.env.REACT_APP_API_KEY
+
+    const downloadWeather = (country) => {
+        //console.log('downloading weather', weather)
+        axios
+            .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.name}`)
+            .then(response => {
+                setWeather(response.data)
+            })
+            .catch(error =>
+                console.log('couldnt download weather data')
+            )
+    }
+
+    downloadWeather(country)
+    console.log(weather)
+
     console.log('country selected: ', country)
     return (
         <div>
@@ -7,10 +28,16 @@ const Country = ({ country }) => {
             area {country.area}
             <br />
             <h4>Languages:</h4>
-            {JSON.stringify(country.languages) }
+            {JSON.stringify(country.languages)}
             <br />
             <br />
-            <img src={country.flags.png} />
+            <img src={country.flags.png} alt="flag of a country" />
+            <h3>Weather in {weather.location.name}</h3>
+            <p>
+                <strong>temperature: </strong>{weather.current.temperature} Celcius<br />
+                <img height='100px' width='100px' alt='type of weather' src={weather.current.weather_icons[0]} /><br />
+                <strong>wind:</strong> {weather.current.wind_speed} mph direction {weather.current.wind_dir}
+            </p>
         </div>
     )
 }
