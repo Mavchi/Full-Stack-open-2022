b4c6b97cad0ad64f3685937c13a7ba890bcaef2d
Aleksander <aleksi.khlebnikov@gmail.com>
2022-11-17 10:17:46 +0200

    exercise params changed slightly. Updated older code slightly changed around. Same working program on front end

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,90 +1,67 @@
-import react from 'react'
 import { useState } from 'react'
-
-const Average = ({ feedback }) => {
-  let sum = feedback.good*1 + feedback.bad*-1
-  let total = feedback.good+feedback.neutral+feedback.bad
-
-  console.log(sum)
-  console.log(total)
-
-  return (
-    <div>
-      average {isNaN(sum/total) ? 0 : sum/total} 
-    </div>
-  )
-}
-
-const Positive = ({ feedback }) => {
-  let positive = feedback.good / (feedback.good+feedback.neutral+feedback.bad)
-
-  return (
-    <div>
-      positive {isNaN(positive) ? 0 : positive}
-    </div>
-  )
-}
-
-const ShowFeedback = ({ feedback }) => {
-
-  return (
-    <div>
-      <h3>Statistics</h3>
-      <div>good {feedback.good}</div>
-      <div>neutral {feedback.neutral}</div>
-      <div>bad {feedback.bad}</div>
-      <div>all {feedback.good+feedback.neutral+feedback.bad}</div>
-      <Average feedback={feedback} />
-      <Positive feedback={feedback} />
-    </div>
-  )
-}
 
 const Button = ({ handleClick, label }) => {
   return (
-    <button onClick={handleClick}>
-      {label}
-    </button>
+    <button onClick={handleClick}>{label}</button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const average = () => (good + neutral + bad) !== 0
+    ? (good + bad * -1) / (good + neutral + bad)
+    : 0
+
+  const positive = () => (good + neutral + bad) !== 0
+    ? good / (good + neutral + bad)
+    : 0
+
+  if (good + neutral + bad === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="average" value={average()} />
+          <StatisticsLine text="positive" value={positive()} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td> 
+      <td>{value}</td>
+    </tr>
   )
 }
 
 const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  })
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleGoodClick= () => {
-    const newFeedback = {
-      ...feedback,
-      good: feedback.good+1
-    }
-    setFeedback(newFeedback)
-  }
-  const handleNeutralClick= () => {
-    const newFeedback = {
-      ...feedback,
-      neutral: feedback.neutral+1
-    }
-    setFeedback(newFeedback)
-  }
-  const handleBadClick= () => {
-    const newFeedback = {
-      ...feedback,
-      bad: feedback.bad+1
-    }
-    setFeedback(newFeedback)
-  }
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
 
   return (
     <div>
-      <h3>give feedback</h3>
-      <Button handleClick={handleGoodClick} label="good" />
-      <Button handleClick={handleNeutralClick} label="neutral" />
-      <Button handleClick={handleBadClick} label="bad" />
-
-      <ShowFeedback feedback={feedback} />
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} label="good" />
+      <Button handleClick={handleNeutral} label="neutral" />
+      <Button handleClick={handleBad} label="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
